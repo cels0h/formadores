6f0e298b03c944c4c0d89448339aa514a2d51846
cels0h <martins.lima.celso@escola.pr.gov.br>
2025-06-23 19:05:05 -0300

    scriptpronto

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function inserirNome() {
-    prompt("Qual o seu nome?");
-    document.querySelector("#nome-usuario");
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector ("#nome-usuario");
     elemento.textContent = nomeUsuario;
 }
