9a3f8d15119e036afb15ee0f575cd161bee6db42
cels0h <martins.lima.celso@escola.pr.gov.br>
2025-06-30 18:34:55 -0300

    lista

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function inserirNome() {
 }
 
 inserirNome();
+
+let linguagens = ["JavaScript","Python","C"];
+// indices: 	       0           1	 2
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
