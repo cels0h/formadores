3470eecb27318967fe2669d2a2742c2e20e3367e
cels0h <martins.lima.celso@escola.pr.gov.br>
2025-07-28 18:38:31 -0300

    .

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ let aluno1 = {
   anoLetivo: "2ºsérie",
   materiasFavoritas: ["Geografia","Programação","Arte"]
 }
+
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]);
