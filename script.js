fbb727b675db8983e52168c6fc89c5d4733cf388
cels0h <martins.lima.celso@escola.pr.gov.br>
2025-07-28 18:30:10 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function inserirNome() {
-    let nomeUsuario = prompt("Qual o seu nome?");
-    let elemento = document.querySelector ("#nome-usuario");
-    elemento.textContent = nomeUsuario;
+function inserirNome(){
+let nomeUsuario = prompt("Qual o seu nome?");
+let elemento = document.querySelector("#nome-usuario");
+elemento.textContent = nomeUsuario;
 }
 
 inserirNome();
 
-let linguagens = ["JavaScript","Python","C"];
-// indices: 	       0           1	 2
-console.log(linguagens[0]) // “JavaScript”
-console.log(linguagens[1]) // “Python”
-console.log(linguagens[2]) // “C”
-
+let linguagens = ["Javascript", "Python", "C"];
 const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
+item.textContent = linguagens[2];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
